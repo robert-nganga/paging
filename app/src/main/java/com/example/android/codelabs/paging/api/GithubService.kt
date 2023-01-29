b8b41e0750c7fccd0bc4542fee93fd3d78b76516
robert-nganga <robertnganga220@gmail.com>
2023-01-29 11:09:15 +0300

    restructured project

--- app/src/main/java/com/example/android/codelabs/paging/api/GithubService.kt
+++ app/src/main/java/com/example/android/codelabs/paging/api/GithubService.kt
@@ -16,19 +16,9 @@
 
 package com.example.android.codelabs.paging.api
 
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import okhttp3.logging.HttpLoggingInterceptor.Level
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-const val IN_QUALIFIER = "in:name,description"
-
-/**
- * Github API communication setup via Retrofit.
- */
 interface GithubService {
     /**
      * Get repos ordered by stars.
@@ -40,22 +30,4 @@ interface GithubService {
         @Query("per_page") itemsPerPage: Int
     ): RepoSearchResponse
 
-    companion object {
-        private const val BASE_URL = "https://api.github.com/"
-
-        fun create(): GithubService {
-            val logger = HttpLoggingInterceptor()
-            logger.level = Level.BASIC
-
-            val client = OkHttpClient.Builder()
-                .addInterceptor(logger)
-                .build()
-            return Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(GithubService::class.java)
-        }
-    }
 }
